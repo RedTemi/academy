98e8b07b3cd2f288f5a729ba2a6e3932cccb5e65
redtemi <tmly16@gmail.com>
2021-02-10 04:45:58 +0100

    added email check

---
+++ checkemail.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin:*");
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: X-PINGOTHER, Content-Type');
+header('Access-Control-Allow-Credentials: true');
+require_once('./main.php');
+$auth = new Auth;
+echo $auth->checkEmail($_POST['email'])? 'valid':'invalid';
