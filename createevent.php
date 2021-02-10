7dac7074097e6da38d41e220c032d3065238589e
redtemi <tmly16@gmail.com>
2021-02-10 14:29:47 +0100

    added event create

---
+++ createevent.php
@@ -0,0 +1,10 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: X-PINGOTHER, Content-Type');
+header('Access-Control-Allow-Credentials: true');
+session_start();
+require './main.php';
+$webinar = new Webinar;
+$load = json_decode($_POST['data'],true);
+echo $webinar->newWebinar($load['name'],$load['description'],$load['uid'],$load['time'],$load['image'],$load['color'],$load['target'])?'done':'failed';
