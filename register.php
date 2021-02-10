4a15cb869c5feef7c15ffb54ec190beb0e1645e2
redtemi <tmly16@gmail.com>
2021-02-10 05:05:59 +0100

    \

--- register.php
+++ register.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin:*");
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: X-PINGOTHER, Content-Type');
 header('Access-Control-Allow-Credentials: true');
+session_start();
 require_once('./main.php');
 $auth = new Auth;
 $load = json_decode($_POST['data'],true);
@@ -23,6 +24,6 @@ $registerx = new Register;
 if (isset($_POST)) {
   if ($auth->checkEmail($email)) {
     $user = new User($fname, $lname, $email, $phone, $password, $is_pharm,$state,$pcn, $psn, $type, $status, $work);
-    $registerx->registerx($user);
+   echo $registerx->registerx($user);
   }
 }
